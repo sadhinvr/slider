49a39aa0b352b0b7712632b11f68b08af3807203
sadhinvr <sadhinvr@gmail.com>
2022-03-06 01:33:31 +0600

    short naming

--- src/slider.js
+++ src/slider.js
@@ -1,106 +1,95 @@
-const $ = document.querySelector.bind(document);
-
-class Carousel {
+class Slider {
     constructor(parent, set) {
-        this.parent = parent;
-        set?.responsive
-            ? (this.responsive = set.responsive)
-            : (this.responsive = { default: { show: 2, slideby: 1 } });
-        !this.responsive.default &&
-            (this.responsive.default = {
-                show: set?.show || 2,
-                slideby: set?.slideby || 1,
+        const z = this;
+        z.p = parent; //parent
+        //responsive
+        set?.res ? (z.res = set.res) : (z.res = { all: { show: 2, to: 1 } });
+        !z.res.all &&
+            (z.res.all = {
+                o: set?.show || 2,
+                to: set?.to || 1,
             });
-        this.show = this.responsive.default.show;
-        this.slideby = this.responsive.default.slideby;
-        this.response();
 
-        this.speed = set?.speed || 500;
-        this.auto = set?.auto || false;
-        this.auto && this.autoPlay();
+        //
+        z.o = z.res.all.show;
+        z.to = z.res.all.to; //slideby
+        z.fr();
 
-        this.styleSheet = document.createElement("style");
-        document.head.appendChild(this.styleSheet);
-        this.styleSheet.sheet.insertRule(`
+        z.sp = set?.speed || 500; //speed
+        z.at = set?.auto || false;
+        z.at &&
+            setInterval(() => {
+                !z.hover && z.fs(z.at?.deraction || 1);
+            }, z.at?.delay || 2000);
+
+        z.ss = document.createElement("style");
+        document.head.appendChild(z.ss);
+        z.ss.sheet.insertRule(`
         .slider-item {
-            min-width: calc((100% / ${this.show}) - 16px);
-            max-width: calc((100% / ${this.show}) - 16px);
+            min-width: calc((100% / ${z.o}) - 16px);
+            max-width: calc((100% / ${z.o}) - 16px);
         }
         `);
-        this.hanResize();
+        z.hanResize();
 
-        this.slide = this.parent.firstElementChild;
-        this.slideItems = this.slide.children;
-        this.total = this.slideItems.length;
-        this.cloneCount = this.getCloneCountForLoop();
-        this.slideCountNew = this.total + this.cloneCount * 2;
-        this.index = this.cloneCount;
-        this.indexMax = Math.max(0, this.slideCountNew - Math.ceil(this.show));
-        this.mainArr = [];
-        this.curIndex = 0;
+        z.s = z.p.firstElementChild; //slide
+        z.i = z.s.children; //children
+        z.t = z.i.length; //total children
+        z.cc = z.gcc(); //clonecount
+        z.cn = z.t + z.cc * 2; //clonecountnew
+        z.x = z.cc; //index
+        z.m = Math.max(0, z.cn - Math.ceil(z.o)); //maxindex
+        z.a = []; //mainarr
+        z.c = 0; //curindex
 
-        this.initStructure();
-        this.slideFun(0);
-        this.events();
+        z.ins();
+        z.fs(0);
+        z.ev();
 
-        
-        const btn = (s=1) => {
-            clearTimeout(this.preBtnTimeOut);
-            this.hover = true;
-            this.preBtnTimeOut = setTimeout(
-                () => (this.hover = false),
-                this.speed * 2
-            );
-            this.slideFun(s);
-        }
+        const btn = (s = 1) => {
+            clearTimeout(z.preBtnTimeOut);
+            z.hover = true;
+            z.preBtnTimeOut = setTimeout(() => (z.hover = false), z.sp * 2);
+            z.fs(s);
+        };
         set?.nxt?.addEventListener("click", () => btn());
-        set?.pre?.addEventListener("click", ()=>btn(-1));
+        set?.pre?.addEventListener("click", () => btn(-1));
     }
 
-    autoPlay() {
-        setInterval(() => {
-            !this.hover && this.slideFun(this.auto?.deraction || 1);
-        }, this.auto?.delay || 2000);
-    }
-
-    getItemsMax() {
+    //getitemsmax
+    gm() {
         const arr = [];
-
-        if (this.show < this.total) {
-            arr.push(this.show);
-        }
-
-        if (!arr.length) {
-            arr.push(0);
-        }
-
+        this.o < this.t && arr.push(this.o);
+        !arr.length && arr.push(0);
         return Math.ceil(Math.max.apply(null, arr));
     }
-
-    getCloneCountForLoop() {
-        let itemsMax = this.getItemsMax(),
-            result = Math.ceil((itemsMax * 5 - this.total) / 2);
+    //getCloneCountForLoop
+    gcc() {
+        let itemsMax = this.gm(),
+            result = Math.ceil((itemsMax * 5 - this.t) / 2);
         result = Math.max(itemsMax, result);
         return result;
     }
 
-    initStructure() {
-        if (this.cloneCount) {
+    //initstucture
+    ins() {
+        const z = this;
+        if (z.cc) {
             let fragmentBefore = document.createDocumentFragment(),
                 fragmentAfter = document.createDocumentFragment();
 
-            this.tempArr = [];
+            z.ta = []; //temp array
 
-            [...this.slideItems].forEach((ele, i) => {
-                ele.dataset.id = i;
-                this.tempArr.push(i);
+            [...z.i].forEach((e, i) => {
+                e.dataset.id = i;
+                z.ta.push(i);
             });
 
-            for (let j = this.cloneCount; j--; ) {
-                let num = j % this.total,
-                    cloneFirst = this.slideItems[num].cloneNode(true),
-                    lastIndex = this.total - 1 - num,
-                    cloneLast = this.slideItems[lastIndex].cloneNode(true);
+            for (let j = z.cc; j--; ) {
+                let num = j % z.t,
+                    cloneFirst = z.i[num].cloneNode(true),
+                    lastIndex = z.t - 1 - num,
+                    cloneLast = z.i[lastIndex].cloneNode(true);
 
                 cloneFirst.classList.add("clone");
                 fragmentAfter.insertBefore(
@@ -112,110 +101,113 @@ class Carousel {
                 fragmentBefore.appendChild(cloneLast);
             }
 
-            this.slide.insertBefore(fragmentBefore, this.slide.firstChild);
-            this.slide.appendChild(fragmentAfter);
+            z.s.insertBefore(fragmentBefore, z.s.firstChild);
+            z.s.appendChild(fragmentAfter);
 
-            [...this.slideItems].forEach((ele) =>
-                this.mainArr.push(ele.dataset.id * 1)
-            );
+            [...z.i].forEach((ele) => z.a.push(ele.dataset.id * 1));
 
-            this.resetPos();
+            z.rp();
         }
     }
 
-    resetPos() {
-        this.curIndex = this.cloneCount;
-        this.slide.style = `transform: translate3d(${
-            (-100 / this.show) * this.curIndex
-        }%, 0px, 0px);transition: all ${this.speed / 1000}s ease 0s`;
+    //resetpos
+    rp() {
+        this.c = this.cc;
+        this.s.style = `transform: translate3d(${
+            (-100 / this.o) * this.c
+        }%, 0px, 0px);transition: all ${this.sp / 1000}s ease 0s`;
     }
 
-    slideFun(sign = 1) {
+    //slidefunction
+    fs(sign = 1) {
         // let r;
-        const temI = this.curIndex + this.slideby * sign;
-        const temI2 = temI + this.slideby * sign;
-        const temRevArr = [...this.tempArr].reverse();
-        this.playBack = (sign = 1) => {
-            console.log(this.curIndex);
-            let pospre = this.mainArr[this.curIndex] - this.slideby,
-                posnxt = this.mainArr[this.curIndex] + this.show + this.slideby;
+        const z = this;
+        const temI = z.c + z.to * sign;
+        const temI2 = temI + z.to * sign;
+        const temRevArr = [...z.ta].reverse();
+        z.playBack = (sign = 1) => {
+            console.log(z.c);
+            let pospre = z.a[z.c] - z.to,
+                posnxt = z.a[z.c] + z.o + z.to;
 
-            posnxt > this.total ? (posnxt = posnxt % this.total) : null;
+            posnxt > z.t ? (posnxt = posnxt % z.t) : null;
             pospre < 0 ? (pospre = temRevArr[-1 * pospre - 1]) : null;
             if (sign == -1) {
-                for (let i = 0; i < this.mainArr.length; i++) {
-                    if (i <= this.curIndex) {
+                for (let i = 0; i < z.a.length; i++) {
+                    if (i <= z.c) {
                         continue;
                     }
                     if (
-                        this.mainArr[i] == this.mainArr[this.curIndex] &&
-                        this.mainArr[i - this.slideby] == pospre
+                        z.a[i] == z.a[z.c] &&
+                        z.a[i - z.to] == pospre
                     ) {
-                        this.curIndex = i;
+                        z.c = i;
                         // console.log(goto);
                         break;
                     }
                 }
             } else {
-                for (let i = this.mainArr.length; i > -1; i--) {
-                    if (i >= this.curIndex) {
+                for (let i = z.a.length; i > -1; i--) {
+                    if (i >= z.c) {
                         continue;
                     }
                     // console.log('kdjk')
                     if (
-                        this.mainArr[i] == this.mainArr[this.curIndex] &&
-                        this.mainArr[i + this.show + this.slideby] == posnxt
+                        z.a[i] == z.a[z.c] &&
+                        z.a[i + z.o + z.to] == posnxt
                     ) {
-                        this.curIndex = i;
+                        z.c = i;
                         break;
                     }
                 }
             }
 
-            this.slide.style = `transform: translate3d(${
-                (-100 / this.show) * this.curIndex
+            z.s.style = `transform: translate3d(${
+                (-100 / z.o) * z.c
             }%, 0px, 0px);transition: all 0s ease 0s`;
         };
 
         if (sign) {
-            if (temI >= 0 && temI <= this.mainArr.length - this.show) {
-                this.curIndex = temI;
-                this.slide.style = `transform: translate3d(${
-                    (-100 / this.show) * this.curIndex
-                }%, 0px, 0px);transition: all ${this.speed / 1000}s ease 0s`;
+            if (temI >= 0 && temI <= z.a.length - z.o) {
+                z.c = temI;
+                z.s.style = `transform: translate3d(${
+                    (-100 / z.o) * z.c
+                }%, 0px, 0px);transition: all ${z.sp / 1000}s ease 0s`;
             }
 
-            // console.log(this.curIndex);
-            if (temI2 < 0 || temI2 > this.mainArr.length - this.show) {
-                setTimeout(() => this.playBack(sign), this.speed);
+            // console.log(z.c);
+            if (temI2 < 0 || temI2 > z.a.length - z.o) {
+                setTimeout(() => z.playBack(sign), z.sp);
             }
         }
-        console.log(this.curIndex);
+        console.log(z.c);
     }
 
-    response() {
+    //reponsive function
+    fr() {
+        const z = this;
         let temTimer,
-            keys = Object.keys(this.responsive)
+            keys = Object.keys(z.res)
                 .filter((k) => k * 1)
                 .map((k) => k * 1)
                 .sort((a, b) => a - b);
 
-        this.hanResize = () => {
-            const index =
+        z.hanResize = () => {
+            const x =
                 [...keys, window.innerWidth]
                     .sort((a, b) => a - b)
                     .findIndex((a) => a == window.innerWidth) - 1;
-            if (index != -1) {
-                const bp = this.responsive[keys[index]];
-                this.show = bp?.show || this.show;
-                this.slideby = bp?.slideby || this.slideby;
+            if (x != -1) {
+                const bp = z.res[keys[x]];
+                z.o = bp?.o || z.o;
+                z.to = bp?.to || z.to;
             } else {
-                this.show = this.responsive.default.show;
-                this.slideby = this.responsive.default.slideby;
+                z.o = z.res.all.o;
+                z.to = z.res.all.to;
             }
 
-            let tempStyle = `calc((100% / ${this.show}) - 16px)`,
-                style = this.styleSheet.sheet.cssRules[0].style;
+            let tempStyle = `calc((100% / ${z.o}) - 16px)`,
+                style = z.ss.sheet.cssRules[0].style;
             style.maxWidth = tempStyle;
             style.minWidth = tempStyle;
         };
@@ -223,27 +215,29 @@ class Carousel {
         window.addEventListener("resize", (e) => {
             temTimer && clearTimeout(temTimer);
             temTimer = setTimeout(() => {
-                this.hanResize();
-                this.resetPos();
+                z.hanResize();
+                z.rp();
             }, 200);
         });
     }
 
-    events() {
-        this.parent.addEventListener("mouseover", () => {
-            this.hover = true;
+    //set events function
+    ev() {
+        const z = this;
+        z.p.addEventListener("mouseover", () => {
+            z.hover = true;
         });
 
         const startSwiping = (e) => {
             if (e.cancelable) e.preventDefault();
             e.stopPropagation();
-            this.hover = true;
-            let react = this.parent.getBoundingClientRect(),
+            z.hover = true;
+            let react = z.p.getBoundingClientRect(),
                 temX = (e.clientX || e.touches[0].clientX) - react.x,
-                temCurInd = this.curIndex,
+                temCurInd = z.c,
                 calPos = (e) => {
-                    this.temXpos =
-                        (-100 / this.show) * temCurInd -
+                    z.temXpos =
+                        (-100 / z.o) * temCurInd -
                         ((temX -
                             (e.clientX || e.touches[0].clientX) -
                             react.x) /
@@ -251,75 +245,55 @@ class Carousel {
                             100;
                 };
             // console.log('touchstart');
-            this.touchMove = (e) => {
+            z.touchMove = (e) => {
                 calPos(e);
-                this.curIndex = Math.round(
-                    Math.abs(this.temXpos / (100 / this.show))
-                );
+                z.c = Math.round(Math.abs(z.temXpos / (100 / z.o)));
 
                 if (
-                    this.temXpos <=
-                        (this.mainArr.length - this.show) *
-                            (-100 / this.show) ||
-                    this.temXpos >= 0
+                    z.temXpos <=
+                        (z.a.length - z.o) * (-100 / z.o) ||
+                    z.temXpos >= 0
                 ) {
-                    // temCurInd=this.curIndex;
-                    // console.log(this.curIndex)
+                    // temCurInd=z.c;
+                    // console.log(z.c)
                     temX = (e.clientX || e.touches[0].clientX) - react.x;
-                    this.playBack(this.temXpos >= 0 ? -1 : 1);
-                    temCurInd = this.curIndex;
+                    z.playBack(z.temXpos >= 0 ? -1 : 1);
+                    temCurInd = z.c;
                     calPos(e);
 
-                    // this.temXpos = (-100 / this.show) * this.curIndex;
-                    console.log((-100 / this.show) * this.curIndex);
+                    // z.temXpos = (-100 / z.o) * z.c;
+                    console.log((-100 / z.o) * z.c);
                 }
 
-                // console.log(this.temXpos)
-                this.slide.style = `transform: translate3d(${this.temXpos}%, 0px, 0px);transition:0s`;
+                // console.log(z.temXpos)
+                z.s.style = `transform: translate3d(${z.temXpos}%, 0px, 0px);transition:0s`;
             };
-            window.addEventListener("touchmove", this.touchMove);
-            window.addEventListener("mousemove", this.touchMove);
+            window.addEventListener("touchmove", z.touchMove);
+            window.addEventListener("mousemove", z.touchMove);
         };
 
         const stopSwiping = (e) => {
-            window.removeEventListener("touchmove", this.touchMove);
-            window.removeEventListener("mousemove", this.touchMove);
-            setTimeout(() => (this.hover = false), this.speed * 2);
-            if (this.temXpos) {
-                this.curIndex = Math.round(
-                    Math.abs(this.temXpos / (100 / this.show))
-                );
-                this.slide.style = `transform: translate3d(${
-                    (-100 / this.show) * this.curIndex
-                }%, 0px, 0px);transition: all ${
-                    this.speed / 1000 / 1.5
-                }s ease 0s`;
+            window.removeEventListener("touchmove", z.touchMove);
+            window.removeEventListener("mousemove", z.touchMove);
+            setTimeout(() => (z.hover = false), z.sp * 2);
+            if (z.temXpos) {
+                z.c = Math.round(Math.abs(z.temXpos / (100 / z.o)));
+                z.s.style = `transform: translate3d(${
+                    (-100 / z.o) * z.c
+                }%, 0px, 0px);transition: all ${z.sp / 1000 / 1.5}s ease 0s`;
             }
         };
 
-        
+        z.p.addEventListener("touchstart", startSwiping);
+        z.s.addEventListener("mousedown", startSwiping);
 
-        this.parent.addEventListener("touchstart", startSwiping);
-        this.slide.addEventListener("mousedown", startSwiping);
+        z.p.addEventListener("touchend", stopSwiping);
+        z.p.addEventListener("mouseup", stopSwiping);
 
-        this.parent.addEventListener("touchend",stopSwiping);
-        this.parent.addEventListener("mouseup",stopSwiping);
-
-        this.parent.addEventListener("mouseleave", () => {
-            this.hover = false;
+        z.p.addEventListener("mouseleave", () => {
+            z.hover = false;
         });
     }
 }
 
-const a = new Carousel($(".carousel"), {
-    show: 3,
-    responsive: {
-        900: {
-            show: 4,
-            slideby: 3,
-        },
-    },
-    pre:$("#pre"),
-    nxt:$("#nxt"),
-    auto:true
-});
+
